9aa49ff4dbaf4665415fe418627ec865c919b177
Ivana Rajkovic <ivana352@gmail.com>
2023-05-04 16:42:10 +0200

    function randomName for names and colors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+// FUNKCIONALNOSTI ZA PRIMANJE I SLANJE PORUKA I RENDERIRANJE
+
+import React from 'react';
 import './App.css';
+import Messages from './Messages';
+
+function randomName() {
+  const adjectives = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
+    "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
+    "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
+    "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
+    "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
+    "ancient", "purple", "lively", "nameless"
+  ];
+  const nouns = [
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
+    "smoke", "star"
+  ];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chat app</h1>
       </header>
     </div>
   );
