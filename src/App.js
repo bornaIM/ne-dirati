2a3a6c7b98118253757ef44d62f9b0f31a5da7fa
Ivana Rajkovic <ivana352@gmail.com>
2023-05-04 18:26:56 +0200

    messages and input components imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// FUNKCIONALNOSTI ZA PRIMANJE I SLANJE PORUKA I RENDERIRANJE
-
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Messages from './Messages';
+import Messages from "./Messages";
+import Input from "./Input";
 
 function randomName() {
   const adjectives = [
@@ -36,14 +35,61 @@ function randomColor() {
   return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Chat app</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    messages: [],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    }
+  }
+
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone("r3L2NUYI7CY6rDnq", {
+      data: this.state.member
+    });
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = {...this.state.member};
+      member.id = this.drone.clientId;
+      this.setState({member});
+    });
+    
+    const room = this.drone.subscribe("observable-room");
+    room.on('data', (data, member) => {
+      const messages = this.state.messages;
+      messages.push({member, text: data});
+      this.setState({messages});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>My Chat App</h1>
+        </div>
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input
+          onSendMessage={this.onSendMessage}
+        />
+      </div>
+    );
+  }
+
+  onSendMessage = (message) => {
+    this.drone.publish({
+      room: "observable-room",
+      message
+    });
+  }
+
 }
 
 export default App;
